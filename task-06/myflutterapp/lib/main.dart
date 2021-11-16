464ed77d1b3f90360324f61768c2cffd4a18b77f
Gk119 <gauthamkrishnanpriya@gmail.com>
2021-11-16 23:48:02 +0530

    Added task-06

---
+++ task-06/myflutterapp/lib/main.dart
@@ -0,0 +1,167 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+
+void main() => runApp(App());
+
+const MaterialColor primaryBlack = MaterialColor(
+  _blackPrimaryValue,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(_blackPrimaryValue),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+const int _blackPrimaryValue = 0xFF000000;
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
+
+    return MaterialApp(
+      title: 'Introduction screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: primaryBlack),
+      home: OnBoardingPage(),
+    );
+  }
+}
+
+class OnBoardingPage extends StatefulWidget {
+  @override
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => HomePage()),
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('resources/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Color(0xFFD6CBC7),
+      imagePadding: EdgeInsets.zero,
+    );
+
+    const int themeColor = 0xFFD6CBC7; //Theme Color
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Color(0xFFD6CBC7),
+      pages: [
+        PageViewModel(
+          title: "YOGA SURGE",
+          body: "Welcome to Yoga world",
+          image: _buildImage('page1.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Healthy Freaks Exercises",
+          body: "Letting go is the hardest asana",
+          image: _buildImage('page2.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Cycling",
+          body:
+              "You cannot always control what goes on outside. But you can always control what goes on inside",
+          image: _buildImage('page3.png'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "Meditation",
+          body: "The longest journey of any person is the journey inward",
+          image: _buildImage('page4.png'),
+          decoration: pageDecoration,
+        ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      showNextButton: false,
+      skipFlex: 0,
+      nextFlex: 0,
+      //rtl: true, // Display as right-to-left
+      skip: const Text('Skip'),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Get Started',
+          style: TextStyle(fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(20),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(0)
+          : const EdgeInsets.fromLTRB(0, 13, 0, 0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeColor: Color(0xFF142428),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Color(themeColor),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome'),
+        backgroundColor: Color(0xFFD6CBC7),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Image.asset('resources/welcome.png',
+                scale: 0.8, height: 228, width: 496),
+            const Text(
+              'Gautham Krishnan',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            const Padding(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                child: Text(
+                  '''\nHello! I am a First year CSE-AI student who has just started learning Flutter and this is my first App. 
+                ''',
+                  textAlign: TextAlign.center,
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
